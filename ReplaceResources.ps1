dfb2a62ff87d978c3c862c39f28676fe6ef91ab1
YukiIsait <yukiisait@outlook.jp>
2024-12-03 19:52:55 +0800

    Style: 脚本参数首字母大写

--- ReplaceResources.ps1
+++ ReplaceResources.ps1
@@ -1,19 +1,19 @@
 param(
     [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
-    [string] $buildDirectory,
+    [string] $BuildDirectory,
     [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
-    [string] $buildVersion
+    [string] $BuildVersion
 )
 
 $workDir = $PSScriptRoot
 $resDir = "$workDir\..\Resources"
 
 # 拷贝图标
-Copy-Item -Force -Recurse -Path "$resDir\FileIcons\*.ico" -Destination "$buildDirectory\CPP\7zip\Archive\Icons"
+Copy-Item -Force -Recurse -Path "$resDir\FileIcons\*.ico" -Destination "$BuildDirectory\CPP\7zip\Archive\Icons"
 
 # 拷贝资源文件
-Copy-Item -Force -Path "$resDir\Format7zF.rc" -Destination "$buildDirectory\CPP\7zip\Bundles\Format7zF\resource.rc"
-Copy-Item -Force -Path "$resDir\Fm.rc" -Destination "$buildDirectory\CPP\7zip\Bundles\Fm\resource.rc"
+Copy-Item -Force -Path "$resDir\Format7zF.rc" -Destination "$BuildDirectory\CPP\7zip\Bundles\Format7zF\resource.rc"
+Copy-Item -Force -Path "$resDir\Fm.rc" -Destination "$BuildDirectory\CPP\7zip\Bundles\Fm\resource.rc"
 
 # 拷贝UI图
-Copy-Item -Force -Path "$resDir\ToolBarIcons\*.bmp" -Destination "$buildDirectory\CPP\7zip\UI\FileManager"
+Copy-Item -Force -Path "$resDir\ToolBarIcons\*.bmp" -Destination "$BuildDirectory\CPP\7zip\UI\FileManager"
