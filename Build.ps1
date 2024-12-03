4ab1213e2002c7ad30961493d74743079d4b27f4
YukiIsait <yukiisait@outlook.jp>
2024-12-03 19:53:15 +0800

    Style: 添加注释

--- Build.ps1
+++ Build.ps1
@@ -2,9 +2,11 @@
 
 $workDir = $PSScriptRoot
 $buildDir = "$workDir\Build"
+
 if (Test-Path $buildDir) {
     Copy-Item -Path "$workDir\ReplaceResources.ps1" -Destination "$buildDir\SubPrepare.ps1"
     & "$buildDir\AutoBuild.ps1" $buildVersion
 } else {
+    # 如果没有初始化子模块则找不到构建脚本
     throw '7-Zip Builder Not Found.'
 }
