754993d8d4d2dab7ae4224022131580c59f23f81
YukiIsait <yukiisait@outlook.jp>
2025-01-14 16:06:16 +0800

    Fix: 修复构建脚本检查

--- Build.ps1
+++ Build.ps1
@@ -3,7 +3,7 @@
 $workDir = $PSScriptRoot
 $buildDir = "$workDir\Build"
 
-if (Test-Path $buildDir) {
+if (Test-Path "$buildDir\*") {
     Copy-Item -Path "$workDir\ReplaceResources.ps1" -Destination "$buildDir\SubPrepare.ps1"
     & "$buildDir\AutoBuild.ps1" $buildVersion
 } else {
